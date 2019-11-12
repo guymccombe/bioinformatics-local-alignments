4b7040fa712851165b76aba007eb094eaf10cef8
unknown <guymccombe@gmail.com>
2019-11-12 19:32:42 +0000

    Started backtrack with Hirschberg's algorithm

--- linearSpaceDynamicProgramming.py
+++ linearSpaceDynamicProgramming.py
@@ -1,34 +1,36 @@
-def findAlignments(sequence1, sequence2):
-    if sequence1 > sequence2:
-        findAlignments(sequence2, sequence1)
-    else:
-        column = [0 for i in range(len(sequence2) + 1)]
-        bestScore = 0
-        coordinateOfBest = (0,0)
+def dynproglin(alphabet, scoringMatrix, sequence1, sequence2):
+    alphabet += '_'
+    score, end, _ = twoColumnSearch(alphabet, scoringMatrix, sequence1, sequence2)
 
-        for i in range(len(sequence1)):
-            print (column)
-            lastCol = column
-            column = [0]
-            for j in range(1, len(sequence2) + 1):
-                score = max([
-                    lastCol[j-1] + getScoreOfMatchingCharactersFromScoringMatrix(sequence1[i], sequence2[j-1]),
-                    column[j-1]  + getScoreOfMatchingCharactersFromScoringMatrix(sequence1[i], '-'),
-                    lastCol[j]   + getScoreOfMatchingCharactersFromScoringMatrix('-', sequence2[j-1]),
-                    0
-                ])
-                column.append(score)
-                if score > bestScore:
-                    bestScore = score
-                    coordinateOfBest = (i, j-1)
+    reversed1, reversed2 = trimAndFlipSequences(sequence1, sequence2, end)
+    _, start, _ = twoColumnSearch(alphabet, scoringMatrix, reversed1, reversed2)
 
-        print(bestScore, coordinateOfBest)
-        #todo backtrack
+    trimmedSequence1, trimmedSequence2 = trimAndFlipSequences(reversed1, reversed2, start)
+    indices = hirschberg(alphabet, scoringMatrix, trimmedSequence1, trimmedSequence2)
+    return score, indices[0], indices[1]
 
+def twoColumnSearch(alphabet, scoringMatrix, sequence1, sequence2):
+    column = [0 for i in range(len(sequence2) + 1)]
+    bestScore = 0
+    coordinateOfBest = (0,0)
 
-def getScoreOfMatchingCharactersFromScoringMatrix(character1, character2):
-    global alphabet, scoringMatrix
+    for i in range(len(sequence1)):
+        lastCol = column
+        column = [0]
+        for j in range(1, len(sequence2) + 1):
+            score = max([
+                lastCol[j-1] + getScoreOfMatchingCharactersFromScoringMatrix(alphabet, scoringMatrix, sequence1[i], sequence2[j-1]),
+                column[j-1]  + getScoreOfMatchingCharactersFromScoringMatrix(alphabet, scoringMatrix, sequence1[i], '_'),
+                lastCol[j]   + getScoreOfMatchingCharactersFromScoringMatrix(alphabet, scoringMatrix, '_', sequence2[j-1]),
+                0
+            ])
+            column.append(score)
+            if score > bestScore:
+                bestScore = score
+                coordinateOfBest = (i, j-1)
+    return bestScore, coordinateOfBest, column
 
+def getScoreOfMatchingCharactersFromScoringMatrix(alphabet, scoringMatrix, character1, character2):
     rowIndex = alphabet.index(character2)
     colIndex = alphabet.index(character1)
     
@@ -37,12 +39,33 @@ def getScoreOfMatchingCharactersFromScoringMatrix(character1, character2):
 
     return out
 
-alphabet = ['A', 'C', 'G', 'T']
-scoringMatrix = [[ 0,-2,-2,-2,-2],
-                 [-2, 1,-1,-1,-1],
-                 [-2,-1, 1,-1,-1],
-                 [-2,-1,-1, 1,-1],
-                 [-2,-1,-1,-1, 1]]
-alphabet.insert(0, '-')
+def trimAndFlipSequences(seq1, seq2, trimPoint):
+    s1 = ''.join(reversed(seq1[:trimPoint[0]+1]))
+    s2 = ''.join(reversed(seq2[:trimPoint[1]+1]))
+    return s1, s2
 
-findAlignments('TAATA','TACTAA')
+def hirschberg(alphabet, scoringMatrix, seq1, seq2):
+    print(seq1, seq2)
+    if len(seq1) < 2 or len(seq2) < 2:
+        return [], []
+
+    else:
+        midX = len(seq1) // 2
+
+        _, _, scoreL = twoColumnSearch(alphabet, scoringMatrix, seq1[:midX+1], seq2)
+        _, _, scoreR = twoColumnSearch(alphabet, scoringMatrix, seq1[midX+1:], seq2)
+
+        sumOfScores = [x+y for x, y in zip(scoreL, scoreR[::-1])]
+        midY = sumOfScores.index(max(sumOfScores))
+
+        left = hirschberg(alphabet, scoringMatrix, seq1[:midX+1], seq2[:midY+1])
+        right = hirschberg(alphabet, scoringMatrix, seq1[midX+1:], seq2[midY+1:])
+        return #todo
+
+a = dynproglin ("ABC", [[1,-1,-2,-1],[-1,2,-4,-1],[-2,-4,3,-2],[-1,-1,-2,0]], "AABBAACA", "CBACCCBA")
+print("Score:   ", a[0])
+print("Indices: ", a[1],a[2])
+
+## Expected output:
+# Score:    5
+# Indices:  [3,5,6][1,2,3]
--- naiveDynamicProgramming.py
+++ naiveDynamicProgramming.py
@@ -1,14 +1,15 @@
-def findAlignments(sequence1, sequence2):
-    alignmentScoreMatrix, alignmentBacktrackMatrix = initialiseMatrices(sequence1, sequence2)
+def dynprog(alphabet, scoringMatrix, sequence1, sequence2):
+    alphabet += '_'
+    alignmentScoreMatrix, alignmentBacktrackMatrix = initialiseMatrices(alphabet, scoringMatrix, sequence1, sequence2)
     score = 0
     coordinateOfBest = (0,0)
 
     for i in range (1, len(alignmentScoreMatrix)):
         for j in range (1, len(alignmentScoreMatrix[0])):
             alignmentScoreMatrix[i][j], alignmentBacktrackMatrix[i][j] = max([
-                (alignmentScoreMatrix[i-1][j-1] + getScoreOfMatchingCharactersFromScoringMatrix(sequence1[i-1], sequence2[j-1]), 'D'),
-                (alignmentScoreMatrix[i-1][ j ] + getScoreOfMatchingCharactersFromScoringMatrix(sequence1[i-1], '-'), 'U'),
-                (alignmentScoreMatrix[ i ][j-1] + getScoreOfMatchingCharactersFromScoringMatrix('-', sequence2[j-1]), 'L'),
+                (alignmentScoreMatrix[i-1][j-1] + getScoreOfMatchingCharactersFromScoringMatrix(alphabet, scoringMatrix, sequence1[i-1], sequence2[j-1]), 'D'),
+                (alignmentScoreMatrix[i-1][ j ] + getScoreOfMatchingCharactersFromScoringMatrix(alphabet, scoringMatrix, sequence1[i-1], '_'), 'U'),
+                (alignmentScoreMatrix[ i ][j-1] + getScoreOfMatchingCharactersFromScoringMatrix(alphabet, scoringMatrix, '_', sequence2[j-1]), 'L'),
                 (0, 'G')
             ])
 
@@ -17,34 +18,29 @@ def findAlignments(sequence1, sequence2):
                 coordinateOfBest = (i,j)
 
     sequence1Indices, sequence2Indices = backtrackAndReturnIndicesOfAlignment(alignmentBacktrackMatrix, coordinateOfBest)
+    return score, sequence1Indices, sequence2Indices
 
-    print(alignmentScoreMatrix)
-    print(alignmentBacktrackMatrix)
-    print(score, sequence1Indices, sequence2Indices)
+def initialiseMatrices(alphabet, scoringMatrix, sequence1, sequence2):
+    alignmentScore = [[-1 for _ in range(len(sequence2) + 1)] for _ in range(len(sequence1) + 1)]
+    backtrackMatrix = [['x' for _ in range(len(sequence2) + 1)] for _ in range(len(sequence1) + 1)]
 
-def initialiseMatrices(sequence1, sequence2):
-    scoringMatrix = [[[] for i in range(len(sequence2) + 1)] for i in range(len(sequence1) + 1)]
-    backtrackMatrix = [[[] for i in range(len(sequence2) + 1)] for i in range(len(sequence1) + 1)]
+    alignmentScore[0][0], backtrackMatrix[0][0] = 0, 'G'
 
-    scoringMatrix[0][0], backtrackMatrix[0][0] = 0, 'G'
-
-    for i in range(1, len (scoringMatrix)):
-        scoringMatrix[i][0], backtrackMatrix[i][0] = max(
-            (scoringMatrix[i-1][0] + getScoreOfMatchingCharactersFromScoringMatrix('-', sequence1[i-1]), 'U'),
+    for i in range(1, len (alignmentScore)):
+        alignmentScore[i][0], backtrackMatrix[i][0] = max(
+            (alignmentScore[i-1][0] + getScoreOfMatchingCharactersFromScoringMatrix(alphabet, scoringMatrix, '_', sequence1[i-1]), 'U'),
             (0, 'G')
         )
 
-    for i in range(1, len (scoringMatrix[0])):
-        scoringMatrix[0][i], backtrackMatrix[0][i] = max (
-            (scoringMatrix[0][i-1] + getScoreOfMatchingCharactersFromScoringMatrix('-', sequence2[i-1]), 'L'),
+    for i in range(1, len (alignmentScore[0])):
+        alignmentScore[0][i], backtrackMatrix[0][i] = max (
+            (alignmentScore[0][i-1] + getScoreOfMatchingCharactersFromScoringMatrix(alphabet, scoringMatrix, '_', sequence2[i-1]), 'L'),
             (0, 'G')
         )
 
-    return scoringMatrix, backtrackMatrix
+    return alignmentScore, backtrackMatrix
 
-def getScoreOfMatchingCharactersFromScoringMatrix(character1, character2):
-    global alphabet, scoringMatrix
-
+def getScoreOfMatchingCharactersFromScoringMatrix(alphabet, scoringMatrix, character1, character2):
     rowIndex = alphabet.index(character2)
     colIndex = alphabet.index(character1)
     
@@ -59,12 +55,11 @@ def backtrackAndReturnIndicesOfAlignment(backtrackMatrix, startCoordinate):
     currentState = backtrackMatrix[i][j]
 
     while (currentState != 'G'):
-        indices1.insert(0, i-1)
-        indices2.insert(0, j-1)
-
         if currentState == 'D':
             i -= 1
             j -= 1
+            indices1.insert(0, i)
+            indices2.insert(0, j)
         elif currentState == 'U':
             i -= 1
         else:
@@ -74,19 +69,37 @@ def backtrackAndReturnIndicesOfAlignment(backtrackMatrix, startCoordinate):
     return indices1, indices2
 
 
+## Expected output:
+# Score:    5
+# Indices:  [3,5,6][1,2,3]
+a = dynprog ("ABC", [[1,-1,-2,-1],[-1,2,-4,-1],[-2,-4,3,-2],[-1,-1,-2,0]], "AABBAACA", "CBACCCBA")
+print("Score:   ", a[0])
+print("Indices: ", a[1],a[2])
 
 
+alphabet = "ABCD"
+matrix = [
+    [ 1,-5,-5,-5,-1],
+    [-5, 1,-5,-5,-1],
+    [-5,-5, 5,-5,-4],
+    [-5,-5,-5, 6,-4],
+    [-1,-1,-4,-4,-9]
+]
 
+# Score:    39
+# Indices:  [5, 6, 7, 8, 9, 10, 11, 12, 18, 19] [0, 1, 5, 6, 11, 12, 16, 17, 18, 19]
+a = dynprog(alphabet, matrix, "AAAAACCDDCCDDAAAAACC", "CCAAADDAAAACCAAADDCCAAAA")
+print("Score:   ", a[0])
+print("Indices: ", a[1],a[2])
 
+# Score:    17
+# Indices:  [2, 6, 11, 14, 17] [0, 1, 2, 3, 4]
+a = dynprog(alphabet, matrix, "AACAAADAAAACAADAADAAA", "CDCDDD")
+print("Score:   ", a[0])
+print("Indices: ", a[1],a[2])
 
-
-
-alphabet = ['A', 'C', 'G', 'T']
-scoringMatrix = [[ 0,-2,-2,-2,-2],
-                 [-2, 1,-1,-1,-1],
-                 [-2,-1, 1,-1,-1],
-                 [-2,-1,-1, 1,-1],
-                 [-2,-1,-1,-1, 1]]
-alphabet.insert(0, '-')
-
-findAlignments('TAATA','TACTAA')
+# Score:    81
+# Indices:  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 40, 41, 42, 43, 44, 45, 46, 47, 48, 50, 51, 52, 53, 54, 55, 56, 57, 58] [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 61, 62, 63, 64, 65, 66, 67, 68, 69]
+a = dynprog(alphabet, matrix, "DDCDDCCCDCAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACCCCDDDCDADCDCDCDCD", "DDCDDCCCDCBCCCCDDDCDBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBDCDCDCDCD")
+print("Score:   ", a[0])
+print("Indices: ", a[1],a[2])
